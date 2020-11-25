966f3bb0a2f2262e3a17ceb0b84a5cae2d626788
jiazheng <2360304192@qq.com>
2020-11-25 12:16:19 +0800

    添加项目文件。

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Core;
+
+namespace App3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Loginin(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new shouye()); 
+        }
+
+        private void Register(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new MainPage());
+            //await label.RelRotateTo(360, 1000);
+            Navigation.PushAsync(new Page1());
+            
+        }
+    }
+}
---
+++ App3/App3/shouye.xaml.cs
@@ -0,0 +1,44 @@
+﻿using App3.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class shouye : ContentPage
+    {
+        public shouye()
+        {
+            InitializeComponent();
+            InitMenuBtn();
+        }
+
+        private void InitMenuBtn()
+        {
+            // 设置子按钮
+            List<SubButton> subs = new List<SubButton>();
+            subs.Add(new SubButton { ImageUrl = "jiaru.png", Action = TestMethod1 });
+            subs.Add(new SubButton { ImageUrl = "guanli.png", Action = TestMethod2});
+
+            // 设置按钮的直径为40px
+            menuBtn.ButtonDiameter = 50;
+            // 设置主按钮的按钮图片
+            menuBtn.SetMenuImage("jiahao.png");
+            // 绑定子按钮
+            menuBtn.SetMenuButton(subs);
+        }
+        private void TestMethod1(object obj, EventArgs args)
+        {
+            Navigation.PushAsync(new jiaru());
+        }
+        private void TestMethod2(object obj, EventArgs args)
+        {
+            Navigation.PushAsync(new manage());
+        }
+    }
+}
